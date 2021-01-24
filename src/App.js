22dbf3f21f14b334e0804e29d9f93a469229a13f
James Hernandez <happjnh@gmail.com>
2021-01-23 17:10:34 -0800

    finised sign in hadlesubmit method

--- src/App.js
+++ src/App.js
@@ -37,7 +37,8 @@ class App extends React.Component {
               id:snapShot.id,
               ...snapShot.data()
             }
-          })
+          });
+          console.log(this.state);
         });
       }
       else{
